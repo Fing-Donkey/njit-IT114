26d8bbcbb2b8ae106e35d69251126018d336d280
Fing-Donkey <53437769+Fing-Donkey@users.noreply.github.com>
2019-09-10 14:28:40 -0400

    Add files via upload

---
+++ Lab2.java
@@ -0,0 +1,31 @@
+
+public class Lab2 {
+	public static void main(String[] args) {
+		String name = null;
+		if("Bob".equals(name)) {
+			System.out.println("Bob equals name");
+		}
+		if(name.equals("Bob")) {
+			System.out.println("Name equals Bob");
+		}
+		if("Bob" == name) {
+			System.out.println("Bob is name");
+		}
+		/*for(int k =0; k<20; k++) {
+			int count =0;
+			float floatcount =0.0f;
+		
+			for(int i =0; i <10; i++) {
+				count++;
+				floatcount +=0.1f;
+			}
+			System.out.println("Count: " + count);
+			System.out.println("Float Count: " + floatcount);
+			if(floatcount ==1) {
+				System.out.print("It's actually 10");
+			}
+		}
+	}*/
+	}
+}
+
